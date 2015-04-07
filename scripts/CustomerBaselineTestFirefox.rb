43cd9a53ca06e13a9bde4919bb7bf8f6a9d556c2
George Chi <george.chi@liferay.com>
2015-04-07 08:41:44 -0700

    Update and rename CustomerBaselineTest.rb to CustomerBaselineTestFirefox.rb

--- scripts/CustomerBaselineTestFirefox.rb
+++ scripts/CustomerBaselineTestFirefox.rb
@@ -4,50 +4,18 @@ require "watir-webdriver"
 require "watir-scroll"
 
 # constants
-portalExt = "/Users/gcliferay/test.txt"
-patchLevel = "/Users/gcliferay/patch.txt"
-pictureAttachment = "/Users/gcliferay/test.png"
+portalExt = "../resources/test-portal-ext.txt"
+patchLevel = "../resources/test-patch-info.txt"
+pictureAttachment = "../resources/test-image.png"
 resultsTokenStart = "Showing "
 resultsTokenEnd = " results."
 ticketsTokenStart = "<div class=\"txt-h1 txt-b\"> "
 ticketsTokenEnd = " </div>"
 
-# ask user to select browser
-puts "Which browser would you like to run this test on?"
-puts "\t Press ENTER to use Chrome [Default]"
-puts "\t ENTER 2 to use Firefox"
-# puts "\t ENTER 3 to use Safari"
-# puts "\t ENTER 4 to use IE"
-STDOUT.flush
-browserType = gets.chomp
-
 # open new browser to log into UA
-if browserType == "1" || browserType == ""
-	puts "Selected: Chrome \n \n"
-	puts "Attempting to open Chrome..."
-	browser = Watir::Browser.new :chrome
-	puts "Successfully opened Chrome"
-elsif browserType == "2"
-	puts "Selected: Firefox \n \n"
-	puts "Attempting to open Firefox..."
-	browser = Watir::Browser.new :firefox
-	puts "Successfully opened Firefox"
-# elsif browserType == "3"
-# 	puts "Selected: Safari \n \n"
-# 	puts "Attempting to open Safari..."
-# 	browser = Watir::Browser.new :safari
-# 	puts "Successfully opened Safari"
-# elsif browserType == "4"
-# 	puts "Selected: IE \n \n"
-# 	puts "Attempting to open IE..."
-# 	browser = Watir::Browser.new :ie
-# 	puts "Successfully opened IE"
-else
-	puts "Invalid selection; using Chrome \n \n"
-	puts "Attempting to open Chrome..."
-	browser = Watir::Browser.new :chrome
-	puts "Successfully opened Chrome"
-end
+puts "Attempting to open Firefox..."
+browser = Watir::Browser.new :firefox
+puts "Successfully opened Firefox"
 
 # ask user to begin test
 puts "\n"
